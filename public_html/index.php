da8473cbccb7b9ce1f5a3aad5f37e0c83fef1585
Kaldari <rkaldari@wikimedia.org>
2019-10-13 21:48:59 -0400

    title detection

--- public_html/index.php
+++ public_html/index.php
@@ -4,5 +4,17 @@ require_once("../vendor/autoload.php");
 require_once("../includes/WikisourceApi.php");
 
 $api = new WikisourceApi();
-$result = $api->getPageContent( "Alice's Adventures in Wonderland" );
-var_dump( $result );
+$indexTitle = "Index:The Life of the Spider.djvu";
+$pageContents = $api->getPageContent( $indexTitle );
+if ( preg_match( "/{{index validated date\|.+\|transcluded=yes}}/", $pageContents ) ||
+	preg_match( "/{{index validated date\|.+\|transcluded=notadv}}/", $pageContents )
+) {
+	preg_match( "/\|Title=\[\[(.+)\]\]/", $pageContents, $matches );
+	if ( !empty( $matches ) ) {
+		$transcludedTitle = $matches[0];
+	} else {
+		echo "No title detected: " . $indexTitle . "\n";
+	}
+} else {
+	echo "Not transcluded: " . $indexTitle . "\n";
+}
